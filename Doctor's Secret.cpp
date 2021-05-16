81eaf435ad09ee64012e093704d61fd4ffb8a39e
Akanksha <ashugaonkar2000@gmail.com>
2021-05-16 22:31:40 +0530

    code

---
+++ Doctor's Secret.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ int a,b;
+ cin>>a>>b;
+if (a<=23 || b>=500 && b<=1000)
+ cout<<"Take Medicine"<<endl;
+ else
+cout<<"Don't take Medicine"<<endl; 
+}
+
