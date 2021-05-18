4dd9a9ee61284c1d5f5c65450aee88624c7c9422
Akanksha <ashugaonkar2000@gmail.com>
2021-05-18 23:42:55 +0530

    code

---
+++ Soblity.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{   int x,a,b; 
+	    cin>>x>>a>>b;
+	    cout<<( a + ( 100 - x ) * b ) * 10<<endl;
+	    
+	}
+	
+}
