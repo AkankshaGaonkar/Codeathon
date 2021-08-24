e3761cba2d14ec4e5adbb0fcc69a10d02415012d
Akanksha <ashugaonkar2000@gmail.com>
2021-08-24 05:50:16 +0530

    add

---
+++ randomNo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    srandtime(0);
+    for(int i=1;i<=2;i++)
+    {
+        n=rand(i)%100;
+        cout<<n;
+    }
+    return 0;
+}
+
