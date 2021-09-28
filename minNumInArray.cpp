9bcfb8d3769f25ff05350fa2524813ba435954dc
Akanksha <ashugaonkar2000@gmail.com>
2021-09-28 22:16:10 +0530

    add

---
+++ minNumInArray.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<=n;i++)
+    {
+        cin>>arr[i];
+    }
+    int min=arr[0];
+    for(int i=1;i<=n;i++)
+    {
+        if(min > arr[i])
+        {
+            min=arr[i];
+        }
+    }
+    cout<<min<<endl;
+    return 0;
+}
